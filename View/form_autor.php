1fc12aca33ee0994b1cb7056d1805891b089ea14
231101 <231101@CPD02-M06>
2025-04-28 15:55:50 -0300

    28/04

--- View/form_autor.php
+++ View/form_autor.php
@@ -4,30 +4,32 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistemas Biblioteca - Cadastro de Aluno </title>
+    <title>Sistemas Biblioteca - Cadastro de Autores </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
 </head>
 <body>
     <div>
         <?php include VIEWS . '/Includes/menu.php' ?>
-        <h1> Lista de Alunos </h1>
-        <a href="/aluno/cadastro"> Novo Aluno </a>
+        <h1> Lista de Autores </h1>
+        <a href="/autor/cadastro"> Novo Autor </a>
         <?= $model->getErrors() ?>
         <table class ="table">
             <thead>
                 <tr>
                     <th scope="col">Id</th>
                     <th scope="col">Nome</th>
-                    <th scope="col">Curso</th>
+                    <th scope="col">Data de Nascimento</th>
+                    <th scope="col">CPF</th>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach($model->rools as $aluno): ?>
+                <?php foreach($model->rools as $autor): ?>
                 <tr>
-                    <td><?= $aluno->Id ?> </td>
-                    <td><a href="/aluno/cadastro?id=<?=Id ?>"><?= $aluno->Nome ?> </a> </td>
-                    <td><?= $aluno->Curso ?> </td>
-                    <td><a href="/aluno/delete?id=<?=$aluno->Id ?>">Remover </a> </td>
+                    <td><?= $autor->Id ?> </td>
+                    <td><a href="/autor/cadastro?id=<?=Id ?>"><?= $autor->Nome ?> </a> </td>
+                    <td><?= $autor->Data_de_Nascimento ?> </td>
+                    <td><?= $autor->CPF ?> </td>
+                    <td><a href="/autor/delete?id=<?=$autor->Id ?>">Remover </a> </td>
                 </tr>
                 <?php endforeach ?>
                 </tbody>
--- View/form_categoria.php
+++ View/form_categoria.php
@@ -4,30 +4,28 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Sistemas Biblioteca - Cadastro de Aluno </title>
+    <title>Sistemas Biblioteca - Categorias </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
 </head>
 <body>
     <div>
         <?php include VIEWS . '/Includes/menu.php' ?>
-        <h1> Lista de Alunos </h1>
-        <a href="/aluno/cadastro"> Novo Aluno </a>
+        <h1> Lista de Categorias </h1>
+        <a href="/categoria/cadastro"> Nova Categoria </a>
         <?= $model->getErrors() ?>
         <table class ="table">
             <thead>
                 <tr>
                     <th scope="col">Id</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Curso</th>
+                    <th scope="col">Descrição</th>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach($model->rools as $aluno): ?>
+                <?php foreach($model->rools as $categoria): ?>
                 <tr>
-                    <td><?= $aluno->Id ?> </td>
-                    <td><a href="/aluno/cadastro?id=<?=Id ?>"><?= $aluno->Nome ?> </a> </td>
-                    <td><?= $aluno->Curso ?> </td>
-                    <td><a href="/aluno/delete?id=<?=$aluno->Id ?>">Remover </a> </td>
+                    <td><?= $categoria->Id ?> </td>
+                    <td><a href="/categoria/cadastro?id=<?=Id ?>"><?= $categoria->Descricao ?> </a> </td>
+                    <td><a href="/categoria/delete?id=<?=$categoria->Id ?>">Remover </a> </td>
                 </tr>
                 <?php endforeach ?>
                 </tbody>
